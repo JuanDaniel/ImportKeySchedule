2c81c78076541a46571bb2fb2b53cd80a2f772ef
Juan Daniel SANTANA <juandanielsantana@gmail.com>
2021-05-24 16:39:07 -0400

    Agregar archivos de proyecto.

---
+++ ImportKeySchedule/Forms/ImportKeySchedule.Designer.cs
@@ -0,0 +1,178 @@
+﻿using ImportKeySchedule;
+
+namespace BBI.JD.Forms
+{
+    partial class ImportKeySchedule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImportKeySchedule));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ExcelPath = new System.Windows.Forms.TextBox();
+            this.btn_Excel = new System.Windows.Forms.Button();
+            this.lst_KeySchedules = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Ok = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chk_Purge = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_ColumnsKeySchedule = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_ColumnsExcel = new System.Windows.Forms.TextBox();
+            this.chk_Overwrite = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // txt_ExcelPath
+            // 
+            resources.ApplyResources(this.txt_ExcelPath, "txt_ExcelPath");
+            this.txt_ExcelPath.Name = "txt_ExcelPath";
+            this.txt_ExcelPath.ReadOnly = true;
+            this.txt_ExcelPath.TextChanged += new System.EventHandler(this.txt_ExcelPath_TextChanged);
+            // 
+            // btn_Excel
+            // 
+            resources.ApplyResources(this.btn_Excel, "btn_Excel");
+            this.btn_Excel.Name = "btn_Excel";
+            this.btn_Excel.UseVisualStyleBackColor = true;
+            this.btn_Excel.Click += new System.EventHandler(this.btn_Excel_Click);
+            // 
+            // lst_KeySchedules
+            // 
+            resources.ApplyResources(this.lst_KeySchedules, "lst_KeySchedules");
+            this.lst_KeySchedules.FormattingEnabled = true;
+            this.lst_KeySchedules.Name = "lst_KeySchedules";
+            this.lst_KeySchedules.SelectedIndexChanged += new System.EventHandler(this.lst_KeySchedules_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // btn_Ok
+            // 
+            resources.ApplyResources(this.btn_Ok, "btn_Ok");
+            this.btn_Ok.Name = "btn_Ok";
+            this.btn_Ok.UseVisualStyleBackColor = true;
+            this.btn_Ok.Click += new System.EventHandler(this.btn_Ok_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btn_Cancel, "btn_Cancel");
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+            // 
+            // chk_Purge
+            // 
+            resources.ApplyResources(this.chk_Purge, "chk_Purge");
+            this.chk_Purge.Name = "chk_Purge";
+            this.chk_Purge.UseVisualStyleBackColor = true;
+            this.chk_Purge.CheckedChanged += new System.EventHandler(this.chk_Purge_CheckedChanged);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // txt_ColumnsKeySchedule
+            // 
+            resources.ApplyResources(this.txt_ColumnsKeySchedule, "txt_ColumnsKeySchedule");
+            this.txt_ColumnsKeySchedule.Name = "txt_ColumnsKeySchedule";
+            this.txt_ColumnsKeySchedule.ReadOnly = true;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // txt_ColumnsExcel
+            // 
+            resources.ApplyResources(this.txt_ColumnsExcel, "txt_ColumnsExcel");
+            this.txt_ColumnsExcel.Name = "txt_ColumnsExcel";
+            this.txt_ColumnsExcel.ReadOnly = true;
+            // 
+            // chk_Overwrite
+            // 
+            resources.ApplyResources(this.chk_Overwrite, "chk_Overwrite");
+            this.chk_Overwrite.Name = "chk_Overwrite";
+            this.chk_Overwrite.UseVisualStyleBackColor = true;
+            // 
+            // ImportKeySchedule
+            // 
+            this.AcceptButton = this.btn_Ok;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Cancel;
+            this.Controls.Add(this.chk_Overwrite);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_ColumnsExcel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_ColumnsKeySchedule);
+            this.Controls.Add(this.chk_Purge);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Ok);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lst_KeySchedules);
+            this.Controls.Add(this.btn_Excel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ExcelPath);
+            this.MaximizeBox = false;
+            this.Name = "ImportKeySchedule";
+            this.Load += new System.EventHandler(this.ImportKeySchedule_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ExcelPath;
+        private System.Windows.Forms.Button btn_Excel;
+        private System.Windows.Forms.ListBox lst_KeySchedules;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Ok;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox chk_Purge;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_ColumnsKeySchedule;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_ColumnsExcel;
+        private System.Windows.Forms.CheckBox chk_Overwrite;
+    }
+}
---
+++ ImportKeySchedule/Forms/ImportKeySchedule.cs
@@ -0,0 +1,511 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using SpreadsheetLight;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImportKeySchedule;
+using System.Reflection;
+using System.Globalization;
+using ImportKeySchedule.Resources;
+
+namespace BBI.JD.Forms
+{
+    public partial class ImportKeySchedule : System.Windows.Forms.Form
+    {
+        private CultureInfo culture;
+        private UIApplication application;
+        private UIDocument uiDoc;
+        private Document document;
+        private List<ViewSchedule> viewSchedules;
+        private ViewSchedule schedule;
+        private List<string> columnsSchedule;
+        private List<string> columnsExcel;
+
+        public ImportKeySchedule(UIApplication application)
+        {
+            culture = Command.CultureByDefault();
+
+            InitializeComponent();
+
+            this.application = application;
+            uiDoc = application.ActiveUIDocument;
+            document = uiDoc.Document;
+        }
+
+        private void ImportKeySchedule_Load(object sender, EventArgs e)
+        {
+            viewSchedules = new FilteredElementCollector(document)
+                        .OfClass(typeof(ViewSchedule))
+                            .Cast<ViewSchedule>()
+                                .Where(x => x.Definition.IsKeySchedule) // Get only Key Schedule
+                                    .OrderBy(x => x.Name)
+                                        .ToList();
+
+            bool found = false;
+
+            for (int i = 0; i < viewSchedules.Count; i++)
+            {
+                ViewSchedule schedule = viewSchedules[i];
+
+                // Remove <Revision Schedule> kind
+                if (schedule.Name.Contains("<Revision Schedule>"))
+                {
+                    viewSchedules.RemoveAt(i--);
+
+                    continue;
+                }
+
+                lst_KeySchedules.Items.Add(schedule.Name);
+
+                // Try to auto-select default Key Schedule
+                if (!found)
+                {
+                    if (found = schedule.Name.Contains("Clave.Programa"))
+                    {
+                        lst_KeySchedules.SelectedItem = schedule.Name;
+                    }
+                }
+            }
+
+            lst_KeySchedules.Enabled = true;
+        }
+
+        private void lst_KeySchedules_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btn_Ok.Enabled = ActiveOk();
+            LoadScheduleStructure();
+        }
+
+        private void txt_ExcelPath_TextChanged(object sender, EventArgs e)
+        {
+            btn_Ok.Enabled = ActiveOk();
+
+            if (!string.IsNullOrEmpty(txt_ExcelPath.Text))
+            {
+                LoadExcelStructure();
+            }
+        }
+
+        private void btn_Excel_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                txt_ExcelPath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void chk_Purge_CheckedChanged(object sender, EventArgs e)
+        {
+            chk_Overwrite.Enabled = !(sender as CheckBox).Checked;
+        }
+
+        private void btn_Ok_Click(object sender, EventArgs e)
+        {
+            if (lst_KeySchedules.SelectedIndex < 0)
+            {
+                MessageBox.Show(
+                    ImportKeyScheduleResource.ResourceManager.GetString("SelectKeyScheduleDesc", culture),
+                    ImportKeyScheduleResource.ResourceManager.GetString("SelectKeySchedule", culture),
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            if (!MatchScheduleStructure())
+            {
+                MessageBox.Show(
+                    ImportKeyScheduleResource.ResourceManager.GetString("UnmatchedScheduleDesc", culture),
+                    ImportKeyScheduleResource.ResourceManager.GetString("UnmatchedSchedule", culture),
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            bool purge = false;
+
+            if (chk_Purge.Checked)
+            {
+                DialogResult dialog = MessageBox.Show(
+                    ImportKeyScheduleResource.ResourceManager.GetString("PurgeDataDesc", culture),
+                    ImportKeyScheduleResource.ResourceManager.GetString("PurgeData", culture),
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                purge = dialog == DialogResult.Yes;
+            }
+
+            if (!ValidPID() && !purge)
+            {
+                DialogResult dialog = MessageBox.Show(
+                    string.Format("{0}\n{1}",
+                        ImportKeyScheduleResource.ResourceManager.GetString("InvalidPIDDesc", culture),
+                        ImportKeyScheduleResource.ResourceManager.GetString("InvalidPIDDesc1", culture)),
+                    ImportKeyScheduleResource.ResourceManager.GetString("InvalidPID", culture),
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button2);
+
+                if (dialog == DialogResult.No)
+                {
+                    return;
+                }
+
+                chk_Overwrite.Enabled = false;
+            }
+
+            bool persisted = true;
+
+            Transaction transaction = null;
+
+            try
+            {
+                transaction = new Transaction(document, "Key Schedule imported");
+
+                transaction.Start();
+
+                if (purge)
+                {
+                    PurgeSchedule();
+                }
+                
+                persisted = PersistData(chk_Overwrite.Enabled & chk_Overwrite.Checked);
+
+                if (persisted)
+                {
+                    transaction.Commit();
+                }
+                else
+                {
+                    transaction.RollBack();
+                }
+            }
+            catch(Exception ex)
+            {
+                if (null != transaction)
+                {
+                    transaction.RollBack();
+                }
+
+                throw ex;
+            }
+
+            if (persisted)
+            {
+                MessageBox.Show(
+                    ImportKeyScheduleResource.ResourceManager.GetString("SuccessfulImportDesc", culture),
+                    ImportKeyScheduleResource.ResourceManager.GetString("SuccessfulImport", culture),
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                this.Close();
+            }
+        }
+
+        private string GetTiTleForm()
+        {
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+
+            return string.Format(
+                "{0} ({1}.{2}.{3}.{4})", 
+                ImportKeyScheduleResource.ResourceManager.GetString("WindowsTitle", culture), 
+                version.Major, version.Minor, version.Build, version.Revision);
+        }
+
+        private bool ActiveOk()
+        {
+            return (lst_KeySchedules.SelectedIndex >= 0 && File.Exists(txt_ExcelPath.Text));
+        }
+
+        private void LoadScheduleStructure()
+        {
+            schedule = viewSchedules.ElementAt(lst_KeySchedules.SelectedIndex);
+
+            columnsSchedule = new List<string>(schedule.Definition.GetFieldCount());
+
+            txt_ColumnsKeySchedule.Clear();
+
+            // Get fields form Key Schedule
+            for (int i = 0; i < columnsSchedule.Capacity; i++)
+            {
+                columnsSchedule.Add(schedule.Definition.GetField(i).GetName());
+
+                txt_ColumnsKeySchedule.Text += string.IsNullOrEmpty(txt_ColumnsKeySchedule.Text) ? "" : ",";
+                txt_ColumnsKeySchedule.Text += columnsSchedule.Last();
+            }
+        }
+
+        private void LoadExcelStructure()
+        {
+            SLDocument sl = null;
+
+            try
+            {
+                sl = new SLDocument(txt_ExcelPath.Text);
+            }
+            catch (Exception ex)
+            {
+                txt_ExcelPath.Clear();
+                txt_ColumnsExcel.Clear();
+
+                MessageBox.Show(
+                    ImportKeyScheduleResource.ResourceManager.GetString("ExceptionExcelFileDesc", culture),
+                    ImportKeyScheduleResource.ResourceManager.GetString("ExceptionExcelFile", culture),
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            if (sl != null)
+            {
+                SLWorksheetStatistics stats = sl.GetWorksheetStatistics();
+
+                columnsExcel = new List<string>();
+
+                txt_ColumnsExcel.Clear();
+
+                // Get fields form Excel
+                for (int i = 1; i <= stats.EndColumnIndex; i++)
+                {
+                    string name = sl.GetCellValueAsString(1, i);
+
+                    if (string.IsNullOrEmpty(name))
+                    {
+                        name = "EMPTY";
+                    }
+
+                    columnsExcel.Add(name);
+
+                    txt_ColumnsExcel.Text += string.IsNullOrEmpty(txt_ColumnsExcel.Text) ? "" : ",";
+                    txt_ColumnsExcel.Text += columnsExcel.Last();
+                }
+            }
+        }
+
+        private bool MatchScheduleStructure()
+        {
+            // Compare number's entries
+            if (columnsSchedule.Count != columnsExcel.Count)
+            {
+                return false;
+            }
+
+            // Compare column name by column name
+            for (int i = 0; i < columnsSchedule.Count; i++)
+            {
+                if (columnsSchedule[i] != columnsExcel[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private List<List<string>> LoadExcelData()
+        {
+            List<List<string>> data = null;
+
+            SLDocument sl = null;
+
+            try
+            {
+                sl = new SLDocument(txt_ExcelPath.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                    ImportKeyScheduleResource.ResourceManager.GetString("ExceptionExcelFileDesc", culture),
+                    ImportKeyScheduleResource.ResourceManager.GetString("ExceptionExcelFile", culture),
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return null;
+            }
+
+            if (sl != null)
+            {
+                data = new List<List<string>>();
+
+                SLWorksheetStatistics stats = sl.GetWorksheetStatistics();
+
+                // Get data form Excel
+                for (int i = 2; i <= stats.EndRowIndex; i++)
+                {
+                    List<string> row = new List<string>();
+
+                    for (int j = 1; j <= stats.EndColumnIndex; j++)
+                    {
+                        string value = sl.GetCellValueAsString(i, j);
+
+                        row.Add(value);
+                    }
+
+                    data.Add(row);
+                }
+            }
+
+            return data;
+        }
+
+        private bool ValidPID()
+        {
+            return columnsSchedule.FindIndex(x => x == "pID") >= 0 && columnsExcel.FindIndex(x => x == "pID") >= 0;
+        }
+
+        private void PurgeSchedule()
+        {
+            int rowCount = schedule.GetTableData().GetSectionData(SectionType.Body).NumberOfRows;
+
+            TableSectionData tsd = schedule.GetTableData().GetSectionData(SectionType.Body);
+
+            using (SubTransaction transaction = new SubTransaction(document))
+            {
+                transaction.Start();
+
+                for (int i = 1; i < rowCount; i++)
+                {
+                    tsd.RemoveRow(i);
+                }
+
+                transaction.Commit();
+            }
+        }
+
+        private bool PersistData(bool overwrite = false)
+        {
+            // Parameters list for each entry
+            Dictionary<string, List<Parameter>> parameters = new Dictionary<string, List<Parameter>>();
+
+            IList<Element> elements = new FilteredElementCollector(document, schedule.Id).ToElements();
+
+            bool invalid = false;
+
+            if (overwrite)
+            {
+                foreach (Element element in elements)
+                {
+                    string pID = null;
+                    List<Parameter> entry = new List<Parameter>();
+
+                    foreach (var key in columnsSchedule)
+                    {
+                        entry.Add(element.LookupParameter(key));
+
+                        if (key == "pID")
+                        {
+                            pID = entry.Last().AsString();
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(pID))
+                    {
+                        if (!parameters.ContainsKey(pID))
+                        {
+                            parameters.Add(pID, entry);
+                        }
+                        else
+                        {
+                            invalid = true;
+                        }
+                    }
+                    else
+                    {
+                        invalid = true;
+                    }
+                }
+            }
+
+            if (invalid)
+            {
+                DialogResult dialog = MessageBox.Show(
+                    string.Format("{0}\n{1}", 
+                        ImportKeyScheduleResource.ResourceManager.GetString("WrongKeyScheduleDataDesc", culture),
+                        ImportKeyScheduleResource.ResourceManager.GetString("WrongKeyScheduleDataDesc1", culture)),
+                    ImportKeyScheduleResource.ResourceManager.GetString("WrongKeyScheduleData", culture), 
+                    MessageBoxButtons.YesNo, 
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button2);
+
+                if (dialog == DialogResult.No)
+                {
+                    return false;
+                }
+            }
+
+            List<List<string>> data = LoadExcelData();
+
+            if (data == null)
+            {
+                return false;
+            }
+
+            TableSectionData tsd = schedule.GetTableData().GetSectionData(SectionType.Body);
+
+            using (SubTransaction transaction = new SubTransaction(document))
+            {
+                transaction.Start();
+
+                int index = -1;
+
+                if (overwrite)
+                {
+                    index = columnsExcel.FindIndex(x => x == "pID");
+                }
+
+                foreach (var entry in data)
+                {
+                    List<Parameter> pEntry = null;
+                    bool nEntry = false;
+
+                    if (overwrite) {
+                        if (parameters.ContainsKey(entry[index]))
+                        {
+                            // Get entry for overwrite values
+                            pEntry = parameters[entry[index]];
+                        }
+                    }
+
+                    if (pEntry == null)
+                    {
+                        // Add new entry
+                        pEntry = new List<Parameter>();
+
+                        tsd.InsertRow(0);
+
+                        Element newElement = new FilteredElementCollector(document, schedule.Id).Last();
+
+                        if (newElement != null)
+                        {
+                            foreach (string key in columnsSchedule)
+                            {
+                                pEntry.Add(newElement.LookupParameter(key));
+                            }
+                        }
+
+                        nEntry = true;
+                    }
+
+                    if (nEntry || overwrite)
+                    {
+                        for (int i = 0; i < entry.Count; i++)
+                        {
+                            pEntry[i].Set(entry[i]);
+                        }
+                    }
+                }
+
+                transaction.Commit();
+            }
+
+            return true;
+        }
+    }
+}
