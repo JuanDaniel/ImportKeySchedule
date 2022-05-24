48be4fafb4d78975d89702f6a5f6009699250f54
Juan Daniel SANTANA <juandanielsantana@gmail.com>
2022-05-24 14:35:44 -0400

    Refactorización del instalador permitiendo la instalación en las versiones >= Revit 2019.

--- Installer/Installer.cs
+++ Installer/Installer.cs
@@ -36,58 +36,53 @@ namespace Installer
 
         private void WriteRevitAddin()
         {
-            foreach (var product in RevitProductUtility.GetAllInstalledRevitProducts())
+            Guid guid = new Guid("93768c10-f819-40b7-bd80-030ef355862f");
+            string assembly = GetAssembly();
+            string fullClassName = "BBI.JD.CrtlApplication";
+            string vendorId = "JDS";
+            string vendorDescription = "Juan Daniel SANTANA";
+
+            foreach (string path in GetRevitVersionsPath())
             {
-                if (product.Version == RevitVersion.Revit2019)
+                string pathAddin = Path.Combine(path, "ImportKeySchedule.addin");
+
+                RevitAddInManifest manifest = File.Exists(pathAddin) ? AddInManifestUtility.GetRevitAddInManifest(pathAddin) : new RevitAddInManifest();
+
+                RevitAddInApplication app = manifest.AddInApplications.FirstOrDefault(x => x.AddInId == guid);
+
+                if (app == null)
                 {
-                    string pathAddin = product.AllUsersAddInFolder + "\\ImportKeySchedule.addin";
-                    Guid guid = new Guid("93768c10-f819-40b7-bd80-030ef355862f");
-                    string assembly = GetAssembly();
-                    string fullClassName = "BBI.JD.CrtlApplication";
-                    string vendorId = "JDS";
-                    string vendorDescription = "Juan Daniel SANTANA";
+                    app = new RevitAddInApplication("ImportKeySchedule", assembly, guid, fullClassName, vendorId);
+                    app.VendorDescription = vendorDescription;
 
-                    RevitAddInManifest manifest = File.Exists(pathAddin) ? AddInManifestUtility.GetRevitAddInManifest(pathAddin) : new RevitAddInManifest();
+                    manifest.AddInApplications.Add(app);
+                }
+                else
+                {
+                    app.Assembly = assembly;
+                    app.FullClassName = fullClassName;
+                }
 
-                    RevitAddInApplication app = manifest.AddInApplications.FirstOrDefault(x => x.AddInId == guid);
-
-                    if (app == null)
-                    {
-                        app = new RevitAddInApplication("ImportKeySchedule", assembly, guid, fullClassName, vendorId);
-                        app.VendorDescription = vendorDescription;
-
-                        manifest.AddInApplications.Add(app);
-                    }
-                    else
-                    {
-                        app.Assembly = assembly;
-                        app.FullClassName = fullClassName;
-                    }
-
-                    if (manifest.Name == null)
-                    {
-                        manifest.SaveAs(pathAddin);
-                    }
-                    else
-                    {
-                        manifest.Save();
-                    }
+                if (manifest.Name == null)
+                {
+                    manifest.SaveAs(pathAddin);
+                }
+                else
+                {
+                    manifest.Save();
                 }
             }
         }
 
         private void DeleteRevitAddin()
         {
-            foreach (var product in RevitProductUtility.GetAllInstalledRevitProducts())
+            foreach (string path in GetRevitVersionsPath())
             {
-                if (product.Version == RevitVersion.Revit2019)
+                string pathAddin = Path.Combine(path, "ImportKeySchedule.addin");
+
+                if (File.Exists(pathAddin))
                 {
-                    string pathAddin = product.AllUsersAddInFolder + "\\ImportKeySchedule.addin";
-                    
-                    if (File.Exists(pathAddin))
-                    {
-                        File.Delete(pathAddin);
-                    }
+                    File.Delete(pathAddin);
                 }
             }
         }
@@ -99,6 +94,35 @@ namespace Installer
             pathDir = pathDir.Remove(pathDir.Length - 1, 1);
 
             return pathDir + "ImportKeySchedule.dll";
+        }
+
+        private List<string> GetRevitVersionsPath()
+        {
+            List<string> paths = new List<string>();
+
+            RevitProduct product = RevitProductUtility.GetAllInstalledRevitProducts()
+                .FirstOrDefault(x => x.Version != RevitVersion.Unknown);
+
+            if (product != null)
+            {
+                DirectoryInfo parent = Directory.GetParent(product.AllUsersAddInFolder);
+
+                int ver;
+
+                foreach (DirectoryInfo version in parent.EnumerateDirectories())
+                {
+                    if (int.TryParse(version.Name, out ver))
+                    {
+                        // The plugin was compiled for versions equal to 2019 and above
+                        if (ver >= 2019)
+                        {
+                            paths.Add(version.FullName);
+                        }
+                    }
+                }
+            }
+
+            return paths;
         }
     }
 }
